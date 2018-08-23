7cb11f6ce35436f13ea43f056045c4b5e4d4335b
ChopCh0p <42531463+ChopCh0p@users.noreply.github.com>
2018-08-23 18:31:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var enter = document.getElementsByClassName('enter');
+
+	function save() {
+		for (var x=data.lenght; x < 3; x++) {
+			data[x] = enter[x].value; //enter[0].value + enter[1].value + enter[2].value;
+		}
+			console.log(data);
+	}
+
+	save();
+
 var tbody = document.getElementsByClassName('tboddy')[0];
 
 for (var i=0; i < data.length; i++){
